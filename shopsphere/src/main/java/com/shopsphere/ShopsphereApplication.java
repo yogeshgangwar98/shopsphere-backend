25734a3e67f177b0c79312ce26d4c9a931d7b2be
Yogesh Gangwar <yogesh.gangwar@happiestminds.com>
2026-09-10 12:17:15 +0530

    Configure PostgreSQL with Docker

---
+++ shopsphere/src/main/java/com/shopsphere/ShopsphereApplication.java
@@ -0,0 +1,13 @@
+package com.shopsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopsphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopsphereApplication.class, args);
+	}
+
+}
---
+++ shopsphere/src/test/java/com/shopsphere/ShopsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopsphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
